7d63c0c9e952a31e66953eb0f5dd40bd81a34e69
nadjibamar <hello@ebabel.eu>
2016-04-12 17:09:07 +0200

    const

--- WeatherProject/index.ios.js
+++ WeatherProject/index.ios.js
@@ -1,9 +1,5 @@
-'use strict';
+const React = require('react-native');
+const { AppRegistry } = React;
 
-var React = require('react-native');
-var {
-  AppRegistry
-} = React;
-
-var WeatherProject = require('./WeatherProject');
+const WeatherProject = require('./WeatherProject');
 AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
