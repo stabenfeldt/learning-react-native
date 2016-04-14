4407a1588cf1695087d6e35d149cfc3c06c7a77d
nadjibamar <hello@ebabel.eu>
2016-04-14 15:22:59 +0200

    improving the weather app

--- WeatherProject/index.android.js
+++ WeatherProject/index.android.js
@@ -1,6 +1,6 @@
 import React from 'react-native';
 const { AppRegistry } = React;
 
-import WeatherProject from './src/components/WeatherProject';
+import WeatherProject from './src/components/weather-project';
 
 AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
