f324f5115bb7805aefcbfdbbb57cf5857a971723
nadjibamar <hello@ebabel.eu>
2016-04-15 18:04:57 +0200

    refactored weather component

--- WeatherProject/index.ios.js
+++ WeatherProject/index.ios.js
@@ -1,6 +1,6 @@
 import React from 'react-native';
 const { AppRegistry } = React;
 
-import WeatherProject from './src/components/weather-project';
+import WeatherProject from './src/components/weather-project/weather-project';
 
 AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
